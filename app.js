1b4d3f81515d3d08b7cbae5a293e986fb8821219
Aviciena Santoso <avi.santoso@gmail.com>
2020-10-05 18:56:44 +0800

    added user field

--- app.js
+++ app.js
@@ -10,23 +10,24 @@ const PORT = process.env.PORT || 3000;
 app.use('messages', new MessageService());
 
 app.service('messages').on('created', message => {
-    console.log(`New message with id: '${message.id}' and message: '${message.text}'`);
+  console.log(`New message created by user ${message.user} with id: '${message.id}' and message: '${message.text}'`);
 })
 
 app.service('messages').on('removed', message => {
-    console.log(`Removed message with id: '${message.id}'`);
+  console.log(`Removed message with id: '${message.id}'`);
 })
 
 app.listen(PORT).on('listening', () => console.log(`Server listening on PORT: ${PORT}`))
 
 const main = async () => {
-    await app.service('messages').create({
-        text: "Server created."
-    });
-    const message = (await app.service('messages').find())[0];
-    await app.service('messages').remove({
-        id: message.id
-    })
+  await app.service('messages').create({
+    text: "Server created.",
+    user: "Admin"
+  });
+  const message = (await app.service('messages').find())[0];
+  await app.service('messages').remove({
+    id: message.id
+  })
 }
 
 main();
--- message-service.js
+++ message-service.js
@@ -1,40 +1,41 @@
 const uuid = require("uuid").v4;
 
 class MessageService {
-    constructor() {
-        this.messages = [];
+  constructor() {
+    this.messages = [];
+  }
+
+  async find() {
+    return this.messages;
+  }
+
+  async create(data) {
+    const now = new Date();
+
+    const newMessage = {
+      id: uuid(),
+      created: now,
+      modified: now,
+      text: data.text,
+      user: data.user,
     }
 
-    async find() {
-        return this.messages;
+    this.messages.push(newMessage);
+
+    return newMessage;
+  }
+
+  async remove(data) {
+    const deleteIdx = this.messages.findIndex((x) => x.id === data.id);
+    if (deleteIdx !== -1) {
+      const toDelete = this.messages[deleteIdx];
+      this.messages.splice(deleteIdx, 1);
+      return toDelete;
     }
-
-    async create(data) {
-        const now = new Date();
-
-        const newMessage = {
-            id: uuid(),
-            created: now,
-            modified: now,
-            text: data.text,
-        }
-
-        this.messages.push(newMessage);
-
-        return newMessage;
+    return {
+      err: `Message not found with id: ${data.id}`
     }
-
-    async remove(data) {
-        const deleteIdx = this.messages.findIndex((x) => x.id === data.id);
-        if (deleteIdx !== -1) {
-            const toDelete = this.messages[deleteIdx];
-            this.messages.splice(deleteIdx, 1);
-            return toDelete;
-        }
-        return {
-            err: `Message not found with id: ${data.id}`
-        }
-    }
+  }
 }
 
 module.exports = MessageService;
